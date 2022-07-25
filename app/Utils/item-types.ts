dbb74c2435f8a8f35c28a29b9291d6833872e7c2
Alex Primo <primo6306@outlook.com>
2022-07-25 14:22:48 -0300

    feat: Create Item types class

---
+++ app/Utils/item-types.ts
@@ -0,0 +1,16 @@
+export const products = {
+  agedBrie: {
+    name: "Aged Brie",
+  },
+  backstagePasses: {
+    name: "Backstage passes to a TAFKAL80ETC concert",
+  },
+  sulfuras: {
+    name: "Sulfuras, Hand of Ragnaros",
+    quality: 80,
+    //quality fixed
+  },
+  conjureds: {
+    name: "Conjured Mana Cake",
+  },
+};
