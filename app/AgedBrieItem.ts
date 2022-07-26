e5117391c8b3f250a31f37c47d03e38e3072a953
Alex Primo <primo6306@outlook.com>
2022-07-26 17:00:06 -0300

    feat: Create Aged Brie Item class

---
+++ app/AgedBrieItem.ts
@@ -0,0 +1,33 @@
+
+import { Item } from "./Item";
+import { VerifyItem } from "./VerifyItem";
+
+export class AgedBrieItem extends Item {
+
+  constructor(
+    sellIn?: number,
+    quality?: number
+  ) {
+    super('Aged Brie', sellIn, quality)
+  }
+
+  execute(item: Item): Item {
+    if (VerifyItem.verifyQuality(item.quality)) {
+      if (item.sellIn <= 50 && item.sellIn > 10){
+        item.quality += 1;
+        item.sellIn--;
+      }else if (item.sellIn <= 10 && item.sellIn > 5) {
+        item.quality += 2;
+        item.sellIn--;
+      } else if (item.sellIn <= 5 && item.sellIn > 0) {
+        item.quality += 3;
+        item.sellIn--;
+      }else{
+        item.quality = 0;
+      }
+      
+    }
+    return item;
+
+  }
+}
