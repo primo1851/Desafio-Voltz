97b1f67264610c85f1fe88bb6c3a0d4794727fde
Alex Primo <primo6306@outlook.com>
2022-07-26 17:02:03 -0300

    test: Create new test cases for Aged Brie class

---
+++ test/jest/AgecBrieItem.spec.ts
@@ -0,0 +1,39 @@
+
+import { Item } from "@/Item";
+import { AgedBrieItem } from "@/AgedBrieItem";
+
+
+
+describe(('Aged Brie test '), () =>{
+const agedBrie = new AgedBrieItem();
+
+
+
+
+it('should return when quality is less then 5', () => {
+    const item: AgedBrieItem = new AgedBrieItem(2, 3);
+    const result: AgedBrieItem = new AgedBrieItem(1, 6);
+
+    const response = agedBrie.execute(item);
+    expect(response).toStrictEqual(result);
+
+  })
+
+  it('should return when quality is less then 10', () => {
+    const item: AgedBrieItem = new AgedBrieItem(10, 3);
+    const result: AgedBrieItem = new AgedBrieItem(9, 5);
+
+    const response = agedBrie.execute(item);
+    expect(response).toStrictEqual(result);
+
+  })
+
+  it('should return when quality is less then 0', () => {
+    const item: AgedBrieItem = new AgedBrieItem(0, 3);
+    const result: AgedBrieItem = new AgedBrieItem(0, 0);
+
+    const response = agedBrie.execute(item);
+    expect(response).toStrictEqual(result);
+
+  })
+})
