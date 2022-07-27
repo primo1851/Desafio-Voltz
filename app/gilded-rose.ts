4d9fd40452771b81b4b603451b8801d72b15f245
Alex Primo <primo6306@outlook.com>
2022-07-26 22:08:13 -0300

    refactor: Refactor Gilded Rose Kata for testing purposes

---
+++ app/gilded-rose.ts
@@ -0,0 +1,49 @@
+import {  AgedBrieItem } from "./AgedBrieItem";
+import { Item } from "./Item";
+import { BackstageItem } from "./BackstageItem";
+import { ConjuredItem } from "./ConjuredItem";
+import { SulfurasItem } from "./SulfurasItem";
+
+
+export class GildedRose {
+  constructor(
+    private readonly agedBrieCaller: AgedBrieItem,
+    private readonly backstageCaller: BackstageItem,
+    private readonly sulfurasCaller: SulfurasItem,
+    private readonly conjuredCaller: ConjuredItem,
+    private readonly itemCaller: Item
+    ) {
+  }
+
+  updateQuality(items: Item[]): Item[] {
+
+    for (let i = 0; i < items.length; i++) {
+
+      switch(items[i].name)
+      {
+        case this.agedBrieCaller.name:
+          this.agedBrieCaller.execute(items[i])
+          break;
+
+        case this.backstageCaller.name:
+          this.backstageCaller.execute(items[i])
+          break;
+        
+        case this.sulfurasCaller.name:
+          this.sulfurasCaller.execute(items[i])
+          break;
+        
+        case this.conjuredCaller.name:
+          this.conjuredCaller.execute(items[i])
+          break;
+
+        default:
+          this.itemCaller.execute(items[i])
+          break;
+
+
+      }
+    }
+    return items;
+  }
+}
