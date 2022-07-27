0d22029cfb6bb363adb9b17b855f97fa56e798a5
Alex Primo <primo6306@outlook.com>
2022-07-27 00:35:54 -0300

    test: Create new test cases for main

--- test/golden-master-text-test.ts
+++ test/golden-master-text-test.ts
@@ -1,32 +1,40 @@
-import { Item, GildedRose } from '../app/gilded-rose';
+import { Item } from "../app/Item";
+import { GildedRose } from "../app/gilded-rose";
+import { AgedBrieItem } from "@/AgedBrieItem";
+import { SulfurasItem } from "@/SulfurasItem";
+import { BackstageItem } from "@/BackstageItem";
+import { ConjuredItem } from "@/ConjuredItem";
 
 const items = [
-  new Item("+5 Dexterity Vest", 10, 20), //
-  new Item("Aged Brie", 2, 0), //
-  new Item("Elixir of the Mongoose", 5, 7), //
-  new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-  new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-  // this conjured item does not work properly yet
-  new Item("Conjured Mana Cake", 3, 6)];
+  new Item("+5 Dexterity Vest", 10, 20), // Randon itens
+  new Item("Elixir of the Mongoose", 5, 7), // Randon itens
+  new SulfurasItem(0), //
+  new AgedBrieItem(2, 0), //
+  new SulfurasItem(-1),
+  new BackstageItem(15, 20),
+  new BackstageItem(10, 49),
+  new BackstageItem(5, 49),
+  new ConjuredItem(3, 6),
+];
 
+const agedBrie = new AgedBrieItem();
+const backstage = new BackstageItem();
+const sulfuras = new SulfurasItem();
+const conjured = new ConjuredItem();
+const foo = new Item();
 
-const gildedRose = new GildedRose(items);
+const gildedRose = new GildedRose(agedBrie, backstage, sulfuras, conjured, foo);
 
 let days: number = 2;
 if (process.argv.length > 2) {
-    days = +process.argv[2];
-  }
+  days = +process.argv[2];
+}
 
 for (let i = 0; i < days; i++) {
-  console.log("-------- day " + i + " --------");
-  console.log("name, sellIn, quality");
-  items.forEach(element => {
-    console.log(element.name + ' ' + element.sellIn + ' ' + element.quality);
-
+  console.log(`-------- day ${i}  --------`);
+  items.forEach((element) => {
+    console.log(element);
   });
   console.log();
-  gildedRose.updateQuality();
+  gildedRose.updateQuality(items);
 }
