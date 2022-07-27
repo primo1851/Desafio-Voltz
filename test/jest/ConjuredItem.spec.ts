a9e51c3770122749c7be1e26117e8678b433314f
Alex Primo <primo6306@outlook.com>
2022-07-27 00:31:26 -0300

    test: Create new test cases for Conjured item class

--- test/jest/ConjuredItem.spec.ts
+++ test/jest/ConjuredItem.spec.ts
@@ -1,11 +1,11 @@
 import { ConjuredItem } from "@/ConjuredItem";
 
 
-describe(('Backsatge teste '), () => {
+describe(('Conjured Items test '), () => {
     const conjured = new ConjuredItem();
 
 
-    it('should return test for cojured items', () => {
+    it('should return quality minus 1', () => {
         const item: ConjuredItem = new ConjuredItem("Conjured Mana Cake",10, 3);
         const result: ConjuredItem = new ConjuredItem("Conjured Mana Cake",9, 1);
 
