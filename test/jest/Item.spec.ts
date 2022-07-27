704cf58bf667e4234ac8a3ff461c367c7d5621e3
Alex Primo <primo6306@outlook.com>
2022-07-27 08:24:15 -0300

    test: Create new test cases for Item class

--- test/jest/Item.spec.ts
+++ test/jest/Item.spec.ts
@@ -15,5 +15,14 @@ it('should return quality minus 1', () => {
 
   })
 
+  it('should return quality 0 when sell in is 0', () => {
+    const item: Item = new Item("foo",0, 6);
+    const result: Item = new Item("foo",0, 0);
+
+    const response = test.execute(item);
+    expect(response).toStrictEqual(result);
+
+  })
+
   
 })
