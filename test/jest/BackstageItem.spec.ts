6a7e3a7e2f19ff23f23b4014e4e818cabecf6917
Alex Primo <primo6306@outlook.com>
2022-07-26 21:51:40 -0300

    test: Create test cases for Backstage  Item class

---
+++ test/jest/BackstageItem.spec.ts
@@ -0,0 +1,36 @@
+
+import { BackstageItem } from "@/BackstageItem";
+
+
+
+describe(('Backsatge teste '), () => {
+    const backsatge = new BackstageItem();
+
+
+    it('should return when quality is less then 5', () => {
+        const item: BackstageItem = new BackstageItem(2, 3);
+        const result: BackstageItem = new BackstageItem(1, 6);
+
+        const response = backsatge.execute(item);
+        expect(response).toStrictEqual(result);
+
+    })
+
+    it('should return when quality is less then 10', () => {
+        const item: BackstageItem = new BackstageItem(10, 3);
+        const result: BackstageItem = new BackstageItem(9, 5);
+
+        const response = backsatge.execute(item);
+        expect(response).toStrictEqual(result);
+
+    })
+
+    it('should return when quality is less then 0', () => {
+        const item: BackstageItem = new BackstageItem(0, 3);
+        const result: BackstageItem = new BackstageItem(0, 0);
+
+        const response = backsatge.execute(item);
+        expect(response).toStrictEqual(result);
+
+    })
+})
